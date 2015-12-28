e88ca6ab8ddd8ec7c9b744aced8d8bb927006af3
BaileyGlen <baileyglen@gmail.com>
2015-12-28 13:13:06 -0500

    table even more complete

--- SurPressesCompiler.m
+++ SurPressesCompiler.m
@@ -51,13 +51,14 @@ for x=1:8
     meanValsByAnimalString{x}= [num2str(meanValsByAnimal((x*2)-1),'%.2f') setstr(177) num2str(meanValsByAnimal(x*2),'%.2f')];
 end
 %% Get nuber of cells in groups
-
+tempData=nan(8,6);
 
 VarList={'LL04','RL04','LL10','RL10'};
 for varIDX=1:4
     tempCount = 1;
+
     cellCounts = cellfun(@length,data.(VarList{varIDX}).pEvt);
-    for x=1:length(data.LL04.pEvt)
+    for x=1:length(data.(VarList{varIDX}).pEvt)
         tempData(varIDX,x)=length(intersect (data.(VarList{varIDX}).DPrime.First3.IncIDs,tempCount:tempCount+cellCounts(x)-1));
         tempData(varIDX+4,x)=length(intersect (data.(VarList{varIDX}).DPrime.First3.DecIDs,tempCount:tempCount+cellCounts(x)-1));
         tempCount=tempCount+cellCounts(x)-1;
